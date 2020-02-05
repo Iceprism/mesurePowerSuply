52ade71798d300523cae89bbd5486f952e08b982
TS <sono@wlan-napt-002.u-aizu.ac.jp>
2020-02-05 09:49:54 +0900

    rename INA226_ADR

--- mesure_powerSupply/mesure_powerSupply.ino
+++ mesure_powerSupply/mesure_powerSupply.ino
@@ -1,12 +1,12 @@
 // https://qiita.com/hurusu1006/items/f493ee4eb9998d5bd740
 #include <Wire.h>
-#define INA226_adr 0b10000000
+#define INA226_ADR 0b10000000
 #define INA226_REG_POWER_CONSUMPTION 0x03
 
 //指定レジスタにデータを書き込む
 void write_INA226_Register(uint8_t reg, uint8_t value) {
-  // 指定したI2Cのスレーブ(MMA8451)に対して送信処理を始める
-  Wire.beginTransmission(INA226_adr);
+  // 指定したI2Cのスレーブ(INA226)に対して送信処理を始める
+  Wire.beginTransmission(INA226_ADR);
 
   //書き込みレジスタを指定する
   Wire.write((uint8_t)reg);
@@ -18,10 +18,10 @@ void write_INA226_Register(uint8_t reg, uint8_t value) {
   Wire.endTransmission();
 }
 
-// MMA8451の指定レジスタからデータを読み出す
+// INA226の指定レジスタからデータを読み出す
 uint8_t readINA226_Register(uint8_t reg) {
-    // 指定したI2Cのスレーブ(MMA8451)に対して送信処理を始める
-    Wire.beginTransmission(INA226_adr);
+    // 指定したI2Cのスレーブ(INA226)に対して送信処理を始める
+    Wire.beginTransmission(INA226_ADR);
 
     // 読み込みたいレジスタを送信
     Wire.write(reg);
@@ -30,7 +30,7 @@ uint8_t readINA226_Register(uint8_t reg) {
     Wire.endTransmission(false);
 
     // INA226に対して1byteのデータを応答データに要求
-    Wire.requestFrom(INA226_adr, 1);
+    Wire.requestFrom(INA226_ADR, 1);
 
     // readで読み取れるバイト数がなければエラー
     if (! Wire.available()) return -1;
@@ -41,18 +41,18 @@ uint8_t readINA226_Register(uint8_t reg) {
 
 // INA226から電力消費を取得する
 int16_t INA226_read(void) {
-  // 指定したI2Cのスレーブ(MMA8451)に対して送信処理を始める
+  // 指定したI2Cのスレーブ(INA226)に対して送信処理を始める
   int16_t power_consumption;
-  Wire.beginTransmission(INA226_adr);
+  Wire.beginTransmission(INA226_ADR);
 
   // データ取得開始アドレスはX軸上位バイトのレジスタ
   Wire.write(INA226_REG_POWER_CONSUMPTION);
 
   // 送信完了(コネクションは維持する)
-  Wire.endTransmission(false); // MMA8451 + friends uses repeated start!!
+  Wire.endTransmission(false); // INA226 + friends uses repeated start!!
 
-  // MMA8451に対して6byteのデータを応答データに要求
-  Wire.requestFrom(INA226_adr, 2);
+  // INA226に対して6byteのデータを応答データに要求
+  Wire.requestFrom(INA226_ADR, 2);
 
   power_consumption = Wire.read(); power_consumption <<= 8; power_consumption |= Wire.read();
 
